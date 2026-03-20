a378e9a05bad5db7866e84838b155a1c2edbeb5a
Lorena Strobel <lorenastrobel97@gmail.com>
2026-03-20 12:50:38 -0400

    Limpando arquivos ignorados e adicionando perfis ao sistema

--- config/application.rb
+++ config/application.rb
@@ -6,10 +6,23 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+# require 'rails/all'
+require "rails"
+# Pick the frameworks you want:
+require "active_model/railtie"
+require "active_job/railtie"
+require "active_record/railtie"
+require "action_controller/railtie"
+require "action_mailer/railtie"
+require "action_view/railtie"
+require "action_cable/engine"
+require "sprockets/railtie"
+
 module TimeToAnswer
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.generators.system_tests = nil
   end
 end
